e1e7d6823c477227c24c74ea9519f68f91284fe5
Harshitp7 <panwar.harshit0602@gmail.com>
2022-08-13 18:54:38 +0530

    added auth contract and functions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,29 @@
 import { useEth } from "./contexts";
 import Demo from "./components/Demo";
 import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import SignIn from "client/src/components/Signin.js";
+import SignUp from "client/src/components/Signup.js";
+import Home from "client/src/components/Home.js";
 
 function App() {
   const { state } = useEth();
+  const email = localStorage.getItem("email");
   console.log({ state })
   return (
     <>
-      <div id="App" >
-        {
-           state?.accounts ? (
-            <div className="container">
-            <Demo />
-          </div>
-           ) : (
-            <div className="container">
-              <div className="loading">
-                <div className="loading-text">
-                  <h1>Loading...</h1>
-                  <p>Please wait while we load the contract.</p>
-                </div>
-              </div>
-            </div>
-           )
-        }
-   
-      </div>
+       <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<SignIn />} />
+          <Route path="/Signup" element={<SignUp />} />
+          <Route
+            path="/Home"
+            element={email ? <Home /> : <Navigate to="/" />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
     </>
   );
 }
---
+++ client/src/components/Home.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Home() {
+const email = localStorage.getItem("email");
+const account = localStorage.getItem("account");
+
+const navigate = useNavigate();
+return (
+	<div>
+	<h3>Your account: {account} </h3>
+	<h3>Your email: {email} </h3>
+	<button
+		style={button}
+		onClick={() => {
+		localStorage.removeItem("email");
+		localStorage.removeItem("account");
+		window.location.reload();
+		}}
+	>
+		{" "}
+		Log out
+	</button>
+	</div>
+);
+}
+const button = {
+width: 100,
+padding: 10,
+borderRadius: 5,
+margin: 10,
+cursor: "pointer",
+fontSize: 17,
+color: "white",
+backgroundColor: "#9D27CD",
+border: "none",
+};
