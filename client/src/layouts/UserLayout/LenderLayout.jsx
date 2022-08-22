ef5309920ce7802d4062a11dd65c4286c07c8994
Ajit-91 <ajitkumar12105@gmail.com>
2022-08-23 01:09:29 +0530

    added lender profile

--- client/src/layouts/UserLayout/LenderLayout.jsx
+++ client/src/layouts/UserLayout/LenderLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Routes, Route, Navigate} from "react-router"
 import Home from '../../pages/Lender/Home'
+import Profile from '../../pages/Lender/Profile'
 
 const LenderLayout = () => {
   return (
     <Routes>
         <Route path="/lender" element={<Home />} />
+        <Route path="/lender/profile/:lenderAddress" element={<Profile />} />
         <Route path='*' element={<Navigate to='/lender' />} />
     </Routes>
   )
--- client/src/pages/Lender/Profile.jsx
+++ client/src/pages/Lender/Profile.jsx
@@ -1,52 +1,52 @@
 import { Button } from '@mui/material';
 import React, { useEffect, useState } from 'react'
-import Layout from '../../components/Layout'
-import { actions, useEth } from '../../contexts';
-import { Form, } from 'react-bootstrap';
-import ProfileCard from '../../components/ProfileCard';
+import { Form } from 'react-bootstrap';
 import { useParams } from 'react-router';
-import { uploadFile, deleteFile } from '../../utils/cloudinaryUtils';
+import Layout from '../../components/Layout'
+import ProfileCard from '../../components/ProfileCard';
 import UpdateIcon from '@mui/icons-material/Update';
+import { actions, useEth } from '../../contexts';
+import { deleteFile, uploadFile } from '../../utils/cloudinaryUtils';
 
 const Profile = () => {
     const { state: { accounts, user, web3, contracts }, dispatch } = useEth();
     const [readOnlyData, setReadOnlyData] = useState([]);
     const [restData, setRestData] = useState({});
     const [loading, setLoading] = useState(true);
-    const { borrowerAddress } = useParams()
+    const { lenderAddress } = useParams()
+    const [interestRate, setInterestRate] = useState('');
+    const [maxPrinciplal, setMaxPrinciplal] = useState('');
 
-    const [annualIncome, setAnnualIncome] = useState('');
     const [imgDetails, setImgDetails] = useState();
 
-    console.log({ annualIncome })
     useEffect(() => {
         const getData = async () => {
             try {
-                let borrowerData;
-                if (borrowerAddress !== accounts[0]) {
-                    borrowerData = await contracts.P2pLending.methods.borrowers(borrowerAddress).call();
+                let lenderData;
+                if (lenderAddress !== accounts[0]) {
+                    lenderData = await contracts.P2pLending.methods.borrowers(lenderAddress).call();
                 }else{
-                    borrowerData = user;
+                    lenderData = user;
                 }
-                console.log({ borrowerData })
-                const balance = await web3.eth.getBalance(borrowerData?.wallet);
+                console.log({ lenderData })
+                const balance = await web3.eth.getBalance(lenderData?.wallet);
                 const balanceInEth = web3.utils.fromWei(balance, 'ether');
-                console.log({ balanceInEth });
+
                 setReadOnlyData([
                     {
                         label: 'Name',
-                        value: borrowerData?.name
+                        value: lenderData?.name
                     },
                     {
                         label: 'Wallet Address',
-                        value: borrowerData?.wallet
+                        value: lenderData?.wallet
                     },
                     {
                         label: 'Wallet Balance (ETH)',
                         value: balanceInEth
                     },
                 ])
-                setRestData(borrowerData);
+                setRestData(lenderData);
                 setLoading(false);
             } catch (error) {
                 alert(error.message);
@@ -54,6 +54,7 @@ const Profile = () => {
         }
         getData();
     }, [])
+
 
     const updateProfile = async () => {
         try {
@@ -66,14 +67,15 @@ const Profile = () => {
                 }
             }
 
-            const borrowerData = await contracts.P2pLending.methods.updateBorrower(
-                Number(annualIncome),
+            const lenderData = await contracts.P2pLending.methods.updateLender(
+                Number(interestRate || restData?.interestRate),
+                Number(maxPrinciplal || restData?.maxPrinciplal),
                 url
             ).send({ from: accounts[0] })
 
-            console.log({ updateRes: borrowerData })
-            if (borrowerData) {
-                const update = await contracts.P2pLending.methods.borrowers(accounts[0]).call()
+            console.log({ updateRes: lenderData })
+            if (lenderData) {
+                const update = await contracts.P2pLending.methods.getLender(accounts[0]).call()
                 dispatch({
                     type: actions.setUser,
                     data: JSON.parse(JSON.stringify(update))
@@ -85,10 +87,11 @@ const Profile = () => {
         }
     }
 
+
     return (
         <Layout>
             {loading ? <div>Loading...</div> : (
-                <ProfileCard spamVotes={restData?.spamVotes || 10} setImgDetails={setImgDetails} walletAddress={borrowerAddress}>
+                <ProfileCard setImgDetails={setImgDetails} walletAddress={lenderAddress}>
                     {
                         readOnlyData.map((item, i) => (
                             <Form.Group key={i} className='mb-3'>
@@ -103,15 +106,24 @@ const Profile = () => {
                             className='fs-6'
                             size='lg'
                             type="number"
-                            onChange={(e) => setAnnualIncome(e.target.value)}
-                            value={annualIncome || restData?.annualIncome}
+                            onChange={(e) => setInterestRate(e.target.value)}
+                            value={interestRate || restData?.interestRate}
                         />
                     </Form.Group>
-                    {borrowerAddress === accounts[0] && (
-                        <Button variant='contained' onClick={updateProfile}>Update &nbsp;<UpdateIcon/></Button>
-                    )}
-                    {borrowerAddress !== accounts[0] && (
-                        <Button color="error" variant='contained' >Mark as Spam</Button>
+
+                    <Form.Group className='mb-3'>
+                        <Form.Label className='text-muted mb-0' >Max Principal</Form.Label>
+                        <Form.Control
+                            className='fs-6'
+                            size='lg'
+                            type="number"
+                            onChange={(e) => setMaxPrinciplal(e.target.value)}
+                            value={maxPrinciplal || restData?.maxPrinciplal}
+                        />
+                    </Form.Group>
+
+                    {lenderAddress === accounts[0] && (
+                        <Button variant='contained' onClick={updateProfile}>Update &nbsp;<UpdateIcon /></Button>
                     )}
                 </ProfileCard>
             )}
