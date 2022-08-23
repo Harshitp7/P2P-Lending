e6cb1a808745afd5f044af2e0cd35a02d987e035
Ajit-91 <ajitkumar12105@gmail.com>
2022-08-24 01:02:08 +0530

    generalised file upload

--- client/src/utils/cloudinaryUtils.js
+++ client/src/utils/cloudinaryUtils.js
@@ -5,7 +5,7 @@ export const uploadFile =  async (file) => {
     const formData = new FormData();
     formData.append('file', file);
     formData.append('upload_preset', 'P2pLending')
-    const response = await fetch(`https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME}/image/upload`, {
+    const response = await fetch(`https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME}/auto/upload`, {
         method: 'POST',
         body: formData,
     });
