6b59fd0ed0dcd1cf1ce0324caa3a42075c69df98
Ajit-91 <ajitkumar12105@gmail.com>
2022-08-31 03:30:28 +0530

    fixed update profile issue

--- client/src/pages/Borrower/RequestDetails.jsx
+++ client/src/pages/Borrower/RequestDetails.jsx
@@ -6,7 +6,7 @@ import Pair from '../../components/Pair'
 import AddTaskIcon from '@mui/icons-material/AddTask';
 import { useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router'
-import { useEth } from '../../contexts'
+import { actions, useEth } from '../../contexts'
 import Loading from '../../components/Loading'
 
 
@@ -21,10 +21,12 @@ const RequestDetails = () => {
 
   const [reqDetails, setReqDetails] = useState({});
   const [paymentDetails, setPaymentDetails] = useState({});
-  const { state: { contracts, accounts, web3 } } = useEth();
+  const { state: { contracts, accounts, web3 }, dispatch } = useEth();
   const [loading, setLoading] = useState(true);
   const [reload, setReload] = useState(false);
-  console.log({paymentDetails})
+  const [backdropLoading, setBackdropLoading] = useState(false);
+
+  console.log({ paymentDetails })
 
   useEffect(() => {
     // fetch request details
@@ -46,17 +48,27 @@ const RequestDetails = () => {
 
   const payBack = async () => {
     try {
-      const res = await contracts['P2pLending'].methods.payBack(requestId).send({ 
-        from: accounts[0], 
-        value : paymentDetails.totalAmount 
+      setBackdropLoading(true);
+      const res = await contracts['P2pLending'].methods.payBack(requestId).send({
+        from: accounts[0],
+        value: paymentDetails.totalAmount
       });
-      if(res?.status){
+      if (res?.status) {
         setReload(prev => !prev);
+        const update = await contracts['P2pLending'].methods.borrowers(accounts[0]).call()
+        const updateObj = Object.assign({}, update);
+        console.log({ updateObj, update })
+        dispatch({
+          type: actions.setUser,
+          data: JSON.parse(JSON.stringify(updateObj))
+        });
+        setBackdropLoading(false);
         alert("Payment sent successfully");
-      }else{
+      } else {
         throw new Error('Payment failed')
       }
     } catch (error) {
+      setBackdropLoading(false);
       alert(error.message || "Something went wrong");
     }
   }
@@ -65,8 +77,9 @@ const RequestDetails = () => {
     <Layout>
       {loading ? <Loading /> : (
         <RequestCommonDetails details={reqDetails}>
+          {backdropLoading && <Loading backdrop />}
           {/* ACCEPTED or COMPLETED */}
-          {(reqDetails?.status === "1"  || reqDetails?.status === "4" ) && (
+          {(reqDetails?.status === "1" || reqDetails?.status === "4") && (
             <>
               <Divider>
                 <Chip label="Payment Details" />
@@ -89,24 +102,24 @@ const RequestDetails = () => {
                 right={`${web3.utils.fromWei(paymentDetails?.totalAmount, 'ether')} ETH`}
               />
 
-            {reqDetails?.status === "1" && (
-              <Box sx={{ mt: 3, display: 'grid', placeItems: 'center' }}>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  endIcon={<AddTaskIcon />}
-                  onClick={payBack}
-                >
-                  Payback
-                </Button>
-              </Box>
-            )}
-            {/* COMPLETED */}
-            {reqDetails?.status === "4" && (
-              <Box sx={{ mt: 3, display: 'grid', placeItems: 'center' }}>
-                <Chip label="Request Completed" />
-              </Box>
-            )}
+              {reqDetails?.status === "1" && (
+                <Box sx={{ mt: 3, display: 'grid', placeItems: 'center' }}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    endIcon={<AddTaskIcon />}
+                    onClick={payBack}
+                  >
+                    Payback
+                  </Button>
+                </Box>
+              )}
+              {/* COMPLETED */}
+              {reqDetails?.status === "4" && (
+                <Box sx={{ mt: 3, display: 'grid', placeItems: 'center' }}>
+                  <Chip label="Request Completed" />
+                </Box>
+              )}
             </>
           )}
         </RequestCommonDetails>
